2cbbe1b18d7537477c927217827e394c48d59233
Hamilton <prof.hamiltonmachiti@usjt.br>
2021-03-12 18:49:32 -0300

    commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//importa os módulos http e express
+const http = require('http');
+const express = require('express');
+//constrói um objeto express
+const app = express();
+//importa o body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//configura a porta do servidor e o coloca em execução.
+const porta = 3000;
+app.set('port', porta);
+const server = http.createServer(app);
+server.listen(3000);
+
+let id = 2;
+let alunos = [{
+        id: 1,
+        nome: "João",
+        fone: "11223344",
+        email: "joao@email.com"
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        fone: "55221133",
+        email: "maria@email.com"
+    }
+];
